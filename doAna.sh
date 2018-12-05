f73ffb22f7d213b5a9612b77b2fbd0eaa90f2b83
MicMa <michael.mahn@tu-dortmund.de>
2018-12-05 23:58:44 +0100

    add doAna.sh (easy syntax for rivet analysis)

---
+++ doAna.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+case $1 in
+  22) ANA=MC_TTBAR;;
+  23) ANA=MC_TTBAR_SINGLEDECAY_23;;
+  24) ANA=MC_TTBAR_SINGELDECAY;;
+  26) ANA=MC_TTBAR_DOUBLEDECAY;; 
+esac
+shift
+
+outputs=()
+for input in $@
+do
+  output="${input%.*}.yoda"
+  if [[ $output == *"-"* ]]; then
+    output="${output%-*}.yoda" 
+  fi
+
+  outputs+=($output)
+
+  echo $output
+  echo $input
+  rivet --analysis $ANA ${input//\\} -o $output --quiet
+
+done
+
+
